d26c8877ab568fb1a6849e45eda2e52390ba0908
Diego Montesanos <diegomontesanosu@gmail.com>
2026-05-06 16:46:08 -0600

    Refactor and improve dataframe utility functions
    Refactor functions for rounding, joining dataframes, calculating RMSE, and derivatives. Added error handling for column checks.

--- comptools.py
+++ comptools.py
@@ -58,40 +58,19 @@ def time_join(dframe1, dframe2, df1_column, df2_column, decimals=3, time_column1
 
 #### Función para obtemer Root Mean Squared Error entre 2 señales
 def RMSE(dframe1, dframe2, df1_column, df2_column, decimals=3, time_column1=0, time_column2=0):
-    df1_columns = dframe1.columns
-    df2_columns = dframe2.columns
     
-    if df1_column in df1_columns and df2_column in df2_columns:
-        dslindex = dframe1.columns.get_loc(df1_column)
-        pssindex = dframe2.columns.get_loc(df2_column)
-
-        dframe1 = dframe1.iloc[:,[time_column1, dslindex]]
-        dframe2 = dframe2.iloc[:,[time_column2, pssindex]]
-
-        dframe1.iloc[:,0] = HUP_round(dframe1.iloc[:,0], decimals)
-        dframe2.iloc[:,0] = HUP_round(dframe2.iloc[:,0], decimals)
-
-        dframe1 = dframe1.drop_duplicates(df1_columns[time_column1], keep='first', ignore_index=True)
-        dframe2 = dframe2.drop_duplicates(df2_columns[time_column2], keep='first', ignore_index=True)
-
-        result = pd.merge(
-            dframe1, dframe2,
-            how='inner',
-            left_on=df1_columns[time_column1],
-            right_on=df2_columns[time_column2]
-        )
-
-        transient = result[result[df1_column]>0.02]
-
-        result['sqr. error'] = (result[df1_column] - result[df2_column])**2
-        return np.sqrt(sum(result['sqr. error']/result.shape[0]))
-
-    else:
-        if df1_column not in df1_columns:
-            print(f'Error: La columna {df1_column} no se encuentra en su respectivo DataFrame')
-        if df2_column not in df2_columns:
-            print(f'Error: La columna {df2_column} no se encuentra en su respectivo DataFrame')
+    if type(df1_column) != str or type(df2_column) != str:
+        print('Error. Solo es posible obtener el error de dos columnas a la vez')
         return np.nan
+    else:
+        joined_result = time_join(dframe1, dframe2, df1_column, df2_column, decimals, time_column1, time_column2)
+
+        if isinstance(joined_result, pd.DataFrame):
+            joined_result['sqr. error'] = (joined_result[df1_column] - joined_result[df2_column])**2
+            return np.sqrt(sum(joined_result['sqr. error']/joined_result.shape[0]))
+        else:
+            return np.nan
+
 ####
 
 #### Función para calcular la derivada de una señal mediante diferenciación finita por método central. Solo soporta pasos más pequeños que 1
